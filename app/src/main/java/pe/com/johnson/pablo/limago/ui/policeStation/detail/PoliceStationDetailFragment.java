763e37fe60ac16ccda63c9d424e0ece544c437ee
pablo-johnson <pablo.88j@gmail.com>
2016-09-04 18:46:36 -0500

    catching ActivityNotFoundException

--- app/src/main/java/pe/com/johnson/pablo/limago/ui/policeStation/detail/PoliceStationDetailFragment.java
+++ app/src/main/java/pe/com/johnson/pablo/limago/ui/policeStation/detail/PoliceStationDetailFragment.java
@@ -1,5 +1,6 @@
 package pe.com.johnson.pablo.limago.ui.policeStation.detail;
 
+import android.content.ActivityNotFoundException;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
@@ -147,7 +148,11 @@ public class PoliceStationDetailFragment extends LimaGoFragment implements OnMap
                 policeStation.getLatitude(), policeStation.getLongitude(), policeStation.getName());
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
         intent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity");
-        startActivity(intent);
+        try {
+            startActivity(intent);
+        } catch (ActivityNotFoundException ex) {
+            //TODO: Do Something
+        }
     }
 
     private void showChooseNumberDialog(final String[] numbers) {
